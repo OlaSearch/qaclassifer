d0aad61c2a0b2d7a7732631cbdc49c77ebac247d
Vinay M <rmdort@gmail.com>
2018-01-18 13:30:42 +0800

    updated reqs

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 import sys
+import os
 from setuptools import setup, find_packages
 name = 'qaclassifier'
-version='1.0.3'
+version='1.0.4'
 package_dir = {name: name}
+
+os.system('pip install git+https://git@github.com/clips/pattern.git@development#egg=Pattern')
 
 if sys.version_info < (3, 3):
   required.append('backports.shutil_get_terminal_size')
@@ -10,8 +13,5 @@ setup(
   name=name,
   version=version,
   license='MIT',
-  package_dir=package_dir,
-  dependency_links=[
-    'git+https://git@github.com/clips/pattern.git@development#egg=Pattern'
-  ]
+  package_dir=package_dir
 )
